5169aab5594e55ac400b858e8b0eede23bf046cb
Jakob F. Gregersen <jfg@bluepp.dk>
2022-01-28 22:04:07 +0100

    feat: Create function for extracting names to binary nodes, add data.csv

---
+++ JakobGregersen_NamePrediction/create_csv.py
@@ -0,0 +1,92 @@
+from array import array
+from os import write
+from tkinter.font import names
+import pandas
+
+location = ("./Maries_navne.xlsx")
+pandaDataframe = pandas.read_excel(location, sheet_name="Ark1")
+
+nodeDictionary = {
+    "A": [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "B": [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "C": [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "D": [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "E": [0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "F": [0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "G": [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "H": [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "I": [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "J": [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "K": [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "L": [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "M": [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "N": [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "O": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "P": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "Q": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
+    "R": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+    "S": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+    "T": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
+    "U": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+    "V": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+    "W": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+    "X": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0],
+    "Y": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0],
+    "Z": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],
+    "Æ": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0],
+    "Ø": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+    "Å": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    "": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+}
+
+def nameToBinaryNodes(str):
+    stringList = str.upper()
+    returnList = list()
+    for i in range(len(stringList)):
+        returnList.append(nodeDictionary[stringList[i]])
+    
+    while(len(returnList)!=10):
+        returnList.append(nodeDictionary[""])
+        
+    returnList = [item for sublist in returnList for item in sublist]
+    return returnList
+# print(nameToBinaryNodes("jakob"))
+
+def createHeaders():
+    nameList = list()
+    for i in range(290):
+        nameList.append("Name1")
+        
+    for i in range(290):
+        nameList.append("Name2")
+        
+    for j in range(len(nameList)):
+        nameList[j] += "Letter" + str(j%10+1)
+        
+    for k in range(len(nameList)):
+        nameList[k] += "Node" + str(k%29+1)
+        
+    return nameList
+# print(createHeaders())
+
+
+Names1 = pandaDataframe['Navn1']
+Names2 = pandaDataframe['Navn2']
+Headers = createHeaders()
+listOfBinNodes = list()
+
+for i in range(len(Names1)):
+    binName1 = nameToBinaryNodes(Names1[i])
+    binName2 = nameToBinaryNodes(Names2[i])
+    binRow = binName1 + binName2
+    listOfBinNodes.append(binRow)
+    
+df = pandas.DataFrame(listOfBinNodes, columns=Headers)
+
+sheetAsCSV = df.to_csv(sep=';')
+
+print("started")
+myfile = open("./data.csv", "w")
+n = myfile.write(sheetAsCSV)
+myfile.close()
+print("finished")
